0a6d2b89eafb16e4879ba1c80ec565c36fd86842
mkodithuwakku <kodithuw@ualberta.ca>
2025-01-16 20:32:44 -0700

    done

--- SpreadPredictor/SpreadCleaner.py
+++ SpreadPredictor/SpreadCleaner.py
@@ -1,32 +1,65 @@
+import numpy as mp
 import pandas as pd
+import random
+import time
 
-# Everything above this line is commented out to skip unnecessary reprocessing
-# Retaining necessary read_csv lines to load data from previous steps
+# List of seasons to scrape
+seasons = [str(season) for season in range(2014,2024)]
+print(f'number of seasons = {len(seasons)}')
 
-# Load the game logs data
+# List of teams
+teams = ['crd', 'atl', 'rav', 'buf', 'car', 'chi', 'cin', 'cle', 'dal', 
+         'gnb', 'htx', 'clt', 'jax', 'kan', 'sdg', 'ram', 'rai', 'mia', 
+         'min', 'nwe', 'nor', 'nyg', 'nyj', 'phi', 'pit', 'sea', 'sfo', 
+          'tam', 'oti', 'was', 'den', 'det']
+
+# Create dataframe
+df = pd.DataFrame()
+
 df = pd.read_csv('nfl_gamelogs_2014-2024.csv')
 print(df.shape)
 
-# Process the game logs data
-nfl_df = df.drop(df.columns[12:], axis=1)  # Drop unnecessary columns
+# Drop unneccesary columns
+nfl_df = df.drop(df.columns[12:], axis = 1)
 nfl_df = nfl_df.drop(nfl_df.columns[5:6], axis=1)
 print(nfl_df.shape)
 
 # Rename columns for clarity
-col_names = {'Unnamed: 4': 'Win', 'Unnamed: 6': 'Home', 'Tm': 'Off_Pts', 'Opp.1': 'Def_Pts'}
+col_names = {'Unnamed: 4':'Win', 'Unnamed: 6':'Home', 'Tm':'Off_Pts', 'Opp.1':'Def_Pts'}
 nfl_df = nfl_df.rename(columns=col_names)
 print(nfl_df.info(verbose=True))
 
-# Load the Vegas lines data
-vegas_df = pd.read_csv('nfl_vegas_lines_2014-2024.csv')
-print(vegas_df.shape)
+# Map 'Opp' to 3 - letter abbreviations
+team_dict = {'Arizona Cardinals':'CRD', 'Atlanta Falcons':'ATL', 'Baltimore Ravens':'RAV',
+             'Buffalo Bills':'BUF', 'Carolina Panthers':'CAR','Chicago Bears':'CHI',
+             'Cincinnati Bengals':'CIN','Cleveland Browns':'CLE', 'Dallas Cowboys':'DAL',
+             'Denver Broncos':'DEN', 'Detroit Lions':'DET', 'Green Bay Packers':'GNB',
+             'Houston Texans':'HTX', 'Indianapolis Colts':'CLT', 'Jacksonville Jaguars':'JAX',
+             'Kansas City Chiefs':'KAN', 'Los Angeles Chargers':'SDG', 'Los Angeles Rams':'RAM',
+             'Las Vegas Raiders':'RAI','Oakland Raiders':'RAI', 'Miami Dolphins':'MIA', 
+             'Minnesota Vikings':'MIN', 'New England Patriots':'NWE', 'New Orleans Saints':'NOR',
+             'New York Giants':'NYG', 'New York Jets':'NYJ', 'Philadelphia Eagles':'PHI',
+             'Pittsburgh Steelers':'PIT', 'St. Louis Rams':'RAM', 'San Diego Chargers':'SDG',
+             'San Francisco 49ers':'SFO', 'Seattle Seahawks':'SEA', 'Tampa Bay Buccaneers':'TAM',
+             'Tennessee Titans':'OTI', 'Washington Commanders':'WAS', 'Washington Football Team':'WAS',
+             'Washington Redskins':'WAS'} 
+nfl_df = nfl_df.replace({'Opp':team_dict})
 
-# Drop unnecessary columns from Vegas lines
+# Convert Wins, OT, and Home to Binary
+nfl_df['Win'] = nfl_df['Win'].apply(lambda x: 1 if x =='W' else 0)
+nfl_df['OT'] = nfl_df['OT'].apply(lambda x: 1 if x =='OT' else 0)
+nfl_df['Home'] = nfl_df['Home'].apply(lambda x: 0 if x =='@' else 1)
+print(nfl_df)
+
+vegas_df = pd.read_csv('nfl_vegas_lines_2014-2024.csv')
+
+print(vegas_df.shape)
+# Drop unnecessary columns
 vegas_df = vegas_df.drop(vegas_df.columns[6:], axis=1)
 print(vegas_df.info(verbose=True))
 
-# Rename columns in Vegas lines
-col_names = {'G#': 'G', 'Over/Under': 'Total'}
+# Rename columns
+col_names = {'G#':'G', 'Over/Under':'Total'}
 vegas_df = vegas_df.rename(columns=col_names)
 print(vegas_df.info(verbose=True))
 
@@ -37,17 +70,16 @@ print(vegas_df.shape)
 # Create Home column based on Opp column
 vegas_df['Home'] = vegas_df['Opp'].apply(lambda x: 0 if x[0] == '@' else 1)
 
-# Remove @ symbol from the 'Opp' column
+# Remove @ symbol
 vegas_df['Opp'] = vegas_df['Opp'].apply(lambda x: x[1:] if x[0] == '@' else x)
 
 # Replace incorrect abbreviations
-abbr_dict = {'OAK': 'RAI', 'LVR': 'RAI', 'STL': 'RAM', 'LAR': 'RAM', 'LAC': 'SDG',
-             'IND': 'CLT', 'HOU': 'HTX', 'BAL': 'RAV', 'ARI': 'CRD', 'TEN': 'OTI'}
-vegas_df = vegas_df.replace({'Opp': abbr_dict})
+abbr_dict = {'OAK':'RAI', 'LVR':'RAI', 'STL':'RAM', 'LAR':'RAM', 'LAC':'SDG',
+             'IND':'CLT', 'HOU':'HTX', 'BAL':'RAV', 'ARI':'CRD', 'TEN':'OTI'}
+vegas_df = vegas_df.replace({'Opp':abbr_dict})
 
-# Ensure 'Home' columns in both DataFrames are of the same type
-nfl_df['Home'] = nfl_df['Home'].astype(int)  # Convert to integer
-vegas_df['Home'] = vegas_df['Home'].astype(int)  # Convert to integer
+print(nfl_df.shape)
+print(vegas_df.shape)
 
 # Merge the data sets
 merged_df = pd.merge(nfl_df, vegas_df, on=['Season', 'Team', 'Opp', 'Home'])
@@ -57,5 +89,13 @@ print(nfl_df.query('Season == 2014 and Team == "BUF"'))
 print(vegas_df.query('Season == 2014 and Team == "BUF"'))
 print(merged_df.query('Season == 2014 and Team == "BUF"'))
 
-# Save the final merged data
 merged_df.to_csv('nfl_pts_and_vegas_2014-2024.csv', index=False)
+
+        
+        
+
+
+
+
+
+
--- SpreadPredictor/spreadAnalysis.py
+++ SpreadPredictor/spreadAnalysis.py
@@ -1,123 +1,161 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 # Import scikit-learn machine learning model
 from sklearn.neighbors import KNeighborsClassifier, NearestNeighbors
+
 # Import scikit-learn metrics and preprocessing libraries
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
 # Load the main dataframe
-nfl_df = pd.read_csv('nfl_scores_2015-2023.csv')
+nfl_df = pd.read_csv('nfl_pts_and_vegas_2014-2024.csv')
 print(nfl_df)
-# Add some columns
-nfl_df['True_Total'] = nfl_df['Tm_Pts'] + nfl_df['Opp_Pts']
+
+# Add True Total, and binary indicators for Over, Under, and Push
+nfl_df['True_Total'] = nfl_df['Off_Pts'] + nfl_df['Def_Pts']
 nfl_df['Over'] = np.where(nfl_df['True_Total'] > nfl_df['Total'], 1, 0)
 nfl_df['Under'] = np.where(nfl_df['True_Total'] < nfl_df['Total'], 1, 0)
 nfl_df['Push'] = np.where(nfl_df['True_Total'] == nfl_df['Total'], 1, 0)
 print(nfl_df)
+
 for season in range(2021, 2024):
     print(nfl_df.query('Season == @season and Week == 1')['Under'].mean())
 # Sort the data by Season, then by Week
 nfl_df = nfl_df.sort_values(by=['Season','Week']).reset_index(drop=True)
+
 # Create and Evaluate a Model for NFL Totals (1 = Under, 0 = Over or Push)
 # Set the dataframe
 df = nfl_df.query('Home == 1').reset_index(drop=True)
+
 # Set the features and the target variable
+# Features is what the algorithm uses to predict the future
+# target is the variable we want to determine in the future
 features = ['Spread','Total']
 target = 'Under'
+
+
 # Iterate over the last three seasons
 for season in [2021, 2022, 2023]:
     # Display the season
     print(f'\nResults for {season}:')
-# Initialize the season aggregates
-y_preds = []
-y_trues = []
-# Iterate over the weeks in the season
-for week in range(1, 19):
-#    Display the current Week
-    print(f' Week {week:>2}:', end=' ')
 
-# Create training set
-train_df = df.query('Season < @season or (Season == @season and Week < @week)')
-# Create testing set
-test_df = df.query('Season == @season and Week == @week and True_Total != Total')
-# Create X_train, y_train, X_test, y_test
-X_train = train_df[features]
-y_train = train_df[target]
-X_test = test_df[features]
-y_test = test_df[target]
-# Set the model
-model = KNeighborsClassifier(n_neighbors=7)
-# Train the model
-clf = model.fit(X_train, y_train)
-# Get the predictions
-y_pred = clf.predict(X_test)
-# Get the true values
-y_true = y_test
-# Display the accuracy score for the current week
-print(f'accuracy score={accuracy_score(y_true, y_pred):.2%}')
-# Update the season aggregates
-y_preds += list(y_pred)
-y_trues += list(y_true)
-# Display the total accuracy score for the current season
-print(f'Season {season}: Total accuracy score={accuracy_score(y_trues, y_preds):.2%}')
-# Display the classification report for the current season
-print(f'\nClassification Report for {season}:')
-print(classification_report(y_trues, y_preds, target_names=['Over','Under']))
-# Display the confusion matrix for the current season
-cm = confusion_matrix(y_trues, y_preds)
-display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Over','Under'])
-display.plot()
-plt.grid(False)
-plt.show()
+    # Initialize the season aggregates
+    y_preds = []
+    y_trues = []
+
+    # Iterate over the weeks in the season
+    for week in range(1, 19):
+
+        # Display the current Week
+        print(f' Week {week:>2}:', end=' ')
+
+        # Create training set
+        train_df = df.query('Season < @season or (Season == @season and Week < @week)')
+        
+        # Create testing set
+        test_df = df.query('Season == @season and Week == @week and True_Total != Total')
+
+        # Create X_train, y_train, X_test, y_test
+        X_train = train_df[features]
+        y_train = train_df[target]
+        X_test = test_df[features]
+        y_test = test_df[target]
+
+        # Set the model
+        model = KNeighborsClassifier(n_neighbors=9)
+
+        # Train the model
+        clf = model.fit(X_train, y_train)
+
+        # Get the predictions
+        y_pred = clf.predict(X_test)
+
+        # Get the true values
+        y_true = y_test
+
+        # Display the accuracy score for the current week
+        print(f'accuracy score={accuracy_score(y_true, y_pred):.2%}')
+
+        # Update the season aggregates
+        y_preds += list(y_pred)
+        y_trues += list(y_true)
+
+    # Display the total accuracy score for the current season
+    print(f'Season {season}: Total accuracy score={accuracy_score(y_trues, y_preds):.2%}')
+
+    # Display the classification report for the current season
+    print(f'\nClassification Report for {season}:')
+    print(classification_report(y_trues, y_preds, target_names=['Over','Under']))
+
+    # Display the confusion matrix for the current season
+    cm = confusion_matrix(y_trues, y_preds)
+    display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Over','Under'])
+    display.plot()
+    plt.grid(False)
+    plt.show()
+
+
 # Get and display the neighbors for games in Season 2023, Week 18
 # Iterate through the test instances and find their nearest neighbors
 index = 0
 for game_index, spread, total in X_test.itertuples():
-# Get the neighbor distances and indices in the dataframe
+    # Get the neighbor distances and indices in the dataframe
     nbr_distance = clf.kneighbors(X_test)[0][index]
-nbr_index = clf.kneighbors(X_test)[1][index]
-index += 1
-# Display the games, the distances, and the neighbors
-print(f'GAME')
-print(df.iloc[[game_index],:][['Season','Week','Tm_Name','Opp_Name','Spread','Total','True_Total','Under']])
-print(f'\nNEAREST NEIGHBORS (distances={[round(value, 2) for value in nbr_distance]})')
-print(df.iloc[nbr_index,:][['Season','Week','Tm_Name','Opp_Name','Spread','Total','True_Total','Under']])
-print('\n')
+    nbr_index = clf.kneighbors(X_test)[1][index]
+    index += 1
 
-# Make predictions for NFL Totals (Season = 2024, Week = 1)
+    # Display the games, the distances, and the neighbors
+    print(f'GAME')
+    print(df.iloc[[game_index],:][['Season','Week','Team','Opp','Spread','Total','True_Total','Under']])
+    print(f'\nNEAREST NEIGHBORS (distances={[round(value, 2) for value in nbr_distance]})')
+    print(df.iloc[nbr_index,:][['Season','Week','Team','Opp','Spread','Total','True_Total','Under']])
+    print('\n')
+
+# Make predictions for NFL Totals (Season = 2024, Divisional Round)
 # Set the dataframe
 df = nfl_df.query('Home == 1').reset_index(drop=True)
+
 # Set the features and the target variable
 features = ['Spread','Total']
 target = 'Under'
+
 # Set the season and the week
 season = 2024
-week = 1
+week = 18
+
 # Create training set
 train_df = df.query('Season < @season or (Season == @season and Week < @week)')
+
 # Create X_train and y_train
 X_train = train_df[features]
 y_train = train_df[target]
+
 # Two-dimensional list with upcoming game data from the home team's perspective
 week1 = [
-['Ravens @ Chiefs', -3.0, 46.5],
-['Packers @ Eagles', -1.5, 48.5],
-['Cardinals @ Bills', -7.0, 48.0],
-['Panthers @ Saints', -4.5, 40.5],
-['Texans @ Colts', +2.0, 48.5]
+['Texans @ Chiefs', -8.5, 41.5],
+['Commanders @ Lions', -9.0, 55.5],
+['Rams @ Eagles', -6.0, 44.0],
+['Ravens @ Bills', +1.0, 51.5]
 ]
+
 # Create X_new dataframe from the upcoming game data
 X_new = pd.DataFrame(week1, columns=['Game','Spread','Total'])
+
 # Set the model
-model = KNeighborsClassifier(n_neighbors=7)
+model = KNeighborsClassifier(n_neighbors=9)
+
 # Fit the classifier
 clf = model.fit(X_train, y_train)
+
 # Make the predictions
 y_pred = clf.predict(X_new[features])
+
 # Add predictions to the dataframe
-X_new['KNC(7)'] = y_pred
-X_new['KNC(7)'] = X_new['KNC(7)'].apply(lambda x: 'Under' if x == 1 else 'Over')
+X_new['KNC(9)'] = y_pred
+X_new['KNC(9)'] = X_new['KNC(9)'].apply(lambda x: 'Under' if x == 1 else 'Over')
+
 # Display the dataframe with the predictions
 print(f'MODEL PREDICTIONS FOR WEEK {week} OF THE {season} NFL SEASON\n')
-print(X_new[['Game','Spread','Total','KNC(7)']])
+print(X_new[['Game','Spread','Total','KNC(9)']])
